6a4fc60808919326297b3d3c9dc6b8b1f6d8cf3d
Eduardo <tkd13000@gmail.com>
2024-11-20 11:07:28 -0600

    When pressing SearchButtonMobile, It hides QuarzoButtonMobile, searchButtonMobile and addQuestionButtonMobile

--- nav.js
+++ nav.js
@@ -1,7 +1,17 @@
 const QuarzoButtonMobile = document.getElementById("QuarzoButtonMobile");
+const searchButtonMobile = document.getElementById("searchButtonMobile");
+const addQuestionButtonMobile = document.getElementById("addQuestionButtonMobile");
 
-
+//It reloads the page
 QuarzoButtonMobile.addEventListener("click", function()
 {
     location.reload();//Reload page
-})
+});
+
+//1.-It hides QuarzoButtonMobile and addQuestionButtonMobile
+searchButtonMobile.addEventListener("click", function()
+{
+    QuarzoButtonMobile.style.display = "none";
+    searchButtonMobile.style.display = "none";
+    addQuestionButtonMobile.style.display = "none";
+});
