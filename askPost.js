56202a3a04d60d44ac969d50774cf03afadd5e77
Eduardo <tkd13000@gmail.com>
2024-12-02 06:02:16 -0600

    Add question button on create post works!

--- askPost.js
+++ askPost.js
@@ -1,8 +1,30 @@
 const askButton = document.getElementById("askButton");
+const cancelQuestionButtonPost = document.getElementById("cancelQuestionButtonPost");
 const floatingLabelAsk = document.getElementById("floatingLabelAsk");
 const postButton = document.getElementById("postButton");
 const floatingLabelPost = document.getElementById("floatingLabelPost");
+const fieldEditPost = document.getElementById("fieldEditPost");
+const addQuestionButtonPost = document.getElementById("addQuestionButtonPost");
 const overlay = document.getElementById("overlay");//Overlay tag(a div)
+
+
+//When fieldEditPost has at least a single character, it will enable addQuestionButtonPost
+fieldEditPost.addEventListener('input', () =>
+{
+    if (fieldEditPost.value.length > 0)
+    {
+        addQuestionButtonPost.style.backgroundColor = "#B92B27";
+    }
+    else
+    {
+        addQuestionButtonPost.style.backgroundColor = "#e55b56";
+    }
+});
+
+cancelQuestionButtonPost.addEventListener("click", function()
+{
+    hideOverlay();
+});
 
 overlay.addEventListener("click", function()
 {
